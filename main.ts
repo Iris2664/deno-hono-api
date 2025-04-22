5789bd7237927388d35c8596d9791d8d4e0443ed
HINATA Ayame <137654183+Iris2664@users.noreply.github.com>
2025-04-22 13:47:10 +0900

    update

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import { Hono } from 'hono';
 import { DOMParser } from '@b-fuze/deno-dom';
 
+interface Bookmark {
+  readonly url: string;
+  readonly title: string;
+}
+
 const app = new Hono();
+
+const kv = await Deno.openKv();
 
 async function fetchHtmlTitle(url: string): Promise<string | undefined> {
   try {
@@ -35,5 +42,24 @@ app.get("/api/title", async (c) => {
   return c.json<{ url: string; title: string }>({ url, title });
 });
 
+app.post("/api/bookmarks", async (c) => {
+  const body = await c.req.parseBody<{ url: string }>();
+  const url = body.url;
+
+  const title = await fetchHtmlTitle(url);
+
+  const result = await kv.set(["bookmark", url], { url, title })
+  return c.json({ result}, 201);
+});
+
+app.get("/api/bookmarks", async (c) => {
+  const bookmarksListItererator =  kv.list<Bookmark>({ prefix: ["bookmark"] });
+  const bookmarks = [];
+  for await ( const bookmark of bookmarksListItererator ) {
+    bookmarks.push(bookmark);
+  }
+  const bookmarksReversed = bookmarks.toReversed();
+  return c.json({bookmarks: bookmarksReversed});
+});
 
 Deno.serve(app.fetch);
